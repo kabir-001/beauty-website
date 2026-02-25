5c36765de33ff5338071549de32bf93de5c37f9d
kabir-001 <167304222+kabir-001@users.noreply.github.com>
2026-02-26 00:25:52 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/beauty-website/',     // ← THIS LINE WAS MISSING
   plugins: [react(), tailwindcss()],
 })
